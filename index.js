5dba370a56d9792813a798b8fa42184c68b06c27
alexc043 <60554853+alexc043@users.noreply.github.com>
2021-05-07 20:18:44 -0400

    Got a little prototype working. It's fun messing around with this Perlin noise stuff

---
+++ index.js
@@ -0,0 +1,34 @@
+var start = 0;
+
+function setup()
+{
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw()
+{
+    background(51);
+    noFill();
+    stroke(255);
+
+    var inc = .005;
+    var scrollSpeed = .03
+    var yoff = start;
+    var y;
+
+    beginShape();
+    for (var x = 0; x < width; x++)
+    {
+        y = noise(yoff) * height;
+        vertex(x, y);
+
+        stroke(135, 206, 235);
+        line(x, 0, x, y);
+        stroke(255);
+        line(x, y, x, y + height / 5);
+
+        yoff += inc;
+    }
+    endShape();
+    start += scrollSpeed;
+}
